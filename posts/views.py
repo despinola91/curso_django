34ce20dbe2c6d9372ed89c70ed316a2b55676760
Dario Espinola <despinola91@gmail.com>
2021-08-20 00:11:49 -0300

    Intro to template system

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 # Django
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Utilities
 from datetime import datetime
@@ -8,34 +8,39 @@ from datetime import datetime
 # Create your views here.
 
 posts = [
-    
     {
-        'name': 'Mont Blac',
-        'user':'Yesica Cortes',
-        'timestamp': datetime.now().strftime('%b %d, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200?image=1036'
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Via Lactea',
-        'user':'Dario Espinola',
-        'timestamp': datetime.now().strftime('%b %d, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200?image=903'
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Nuevo auditorio',
-        'user':'testing',
-        'timestamp': datetime.now().strftime('%b %d, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200?image=1076'
-    },
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 def list_posts(request):
     """List existing posts."""
-    content = []
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <figure><img src="{picture}"/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    
+    context = {
+        'posts': posts
+    }
+    return render(request, 'feed.html', context)
