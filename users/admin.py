5f98aaca057a1faf782bdf902f36893c0f5be0cf
Dario Espinola <despinola91@gmail.com>
2021-08-24 23:58:55 -0300

    Working with admin dashboard and customizing the detail

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,9 @@
 
 # Django
 from django.contrib import admin
+from django.contrib.admin.options import BaseModelAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin    
+from django.contrib.auth.models import User
 
 # Models
 from users.models import Profile
@@ -31,3 +34,44 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__is_active', 
         'user__is_staff'
     )
+    
+    fieldsets = (
+        # First group of fields
+        ('Profile', {
+            'fields': (('user', 'picture'),),
+        }),
+        
+        # Second group of fields
+        ('Extra info', {
+            'fields': ('website', 'phone_number', 'biography')
+        }),
+        
+        # Third group of fields
+        ('Metadata', {
+            'fields': (('created', 'modified'),)
+        })
+    )
+    
+    readonly_fields = ('created', 'modified')
+    
+
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users"""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+    
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin"""
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+    
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
