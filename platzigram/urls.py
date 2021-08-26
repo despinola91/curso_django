630c5122e3f053c759533a8e6b3d8c1702affaa2
Dario Espinola <despinola91@gmail.com>
2021-08-25 23:07:27 -0300

    Creating post model

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +27,5 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>', local_views.say_hi),
     
     path('posts/', posts_views.list_posts),
-    
-]
+        
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Posts Admin model"""
+
+    list_display = ('pk', 'user', 'title', 'photo')
+    list_display_links = ('pk', 'user')
+    list_editable = ('title', 'photo',)
+    list_filter = ('created', 'modified')
+    search_fields = ('user__username', 'title')
+    readonly_fields = ('created', 'modified')
